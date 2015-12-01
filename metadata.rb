8920da63d048fd4982f7626f9b9caf074509acdb
Gavin Staniforth <gavin@usemarkup.com>
2015-12-01 22:25:22 +0000

    Init

---
+++ metadata.rb
@@ -0,0 +1,5 @@
+name             'wkhtmltopdf'
+license          'All rights reserved'
+description      'wkhtmltopdf'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,16 @@
+pkgs = ['xorg-x11-fonts-75dpi', 'xorg-x11-fonts-Type1']
+pkgs.each do |pkg|
+    package pkg do
+        action :install
+    end
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/wkhtmltox-0.12.2_linux-centos6-amd64.rpm" do
+    source "http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-centos6-amd64.rpm"
+    action :create
+end
+
+rpm_package "wkhtmltopdf" do
+    source "#{Chef::Config[:file_cache_path]}/wkhtmltox-0.12.2_linux-centos6-amd64.rpm"
+    action :install
+end
