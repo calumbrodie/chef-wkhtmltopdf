046b1953bc2cfe30e4dfad60e64abbc9499d75b1
Gavin Staniforth <gavin@usemarkup.com>
2015-12-02 11:36:24 +0000

    Added the correct logic for killing the install if already installed

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,10 @@
 remote_file "#{Chef::Config[:file_cache_path]}/wkhtmltox-0.12.2_linux-centos6-amd64.rpm" do
     source "http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-centos6-amd64.rpm"
+    not_if "which wkhtmltopdf"
     action :create_if_missing
 end
 
 package "#{Chef::Config[:file_cache_path]}/wkhtmltox-0.12.2_linux-centos6-amd64.rpm" do
-    not_if "which wkhtmltopdf"
+    subscribes :create_if_missing, resources(:remote_file => "#{Chef::Config[:file_cache_path]}/wkhtmltox-0.12.2_linux-centos6-amd64.rpm")
     action :install
 end
