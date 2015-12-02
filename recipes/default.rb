00e0c225dc07a01a59acd016eb5d07f809e8e766
Gavin Staniforth <gavin@usemarkup.com>
2015-12-02 15:17:52 +0000

    attempt to only run install when required

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,15 @@
+ruby_block 'install-wkhtmltopdf' do
+  block do
+    package "#{Chef::Config[:file_cache_path]}/wkhtmltox-0.12.2_linux-centos6-amd64.rpm" do
+        action :nothing
+    end
+  end
+  action :nothing
+end
+
 remote_file "#{Chef::Config[:file_cache_path]}/wkhtmltox-0.12.2_linux-centos6-amd64.rpm" do
     source "http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-centos6-amd64.rpm"
     not_if "which wkhtmltopdf"
     action :create_if_missing
-end
-
-package "#{Chef::Config[:file_cache_path]}/wkhtmltox-0.12.2_linux-centos6-amd64.rpm" do
-    subscribes :create_if_missing, resources(:remote_file => "#{Chef::Config[:file_cache_path]}/wkhtmltox-0.12.2_linux-centos6-amd64.rpm")
-    action :install
+    notifies :install, 'ruby_block[install-wkhtmltopdf]', :immediately
 end
