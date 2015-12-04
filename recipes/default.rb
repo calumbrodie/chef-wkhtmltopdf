ca9a8efe1711002147b0afc392a6f236c17bdce2
Gavin Staniforth <gsdevme@users.noreply.github.com>
2015-12-04 10:11:54 +0000

    Run

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,12 +4,12 @@ ruby_block 'install-wkhtmltopdf' do
         action :install
     end
   end
-  action :install
+  action :nothing
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/wkhtmltox-0.12.2_linux-centos6-amd64.rpm" do
     source "http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-centos6-amd64.rpm"
     not_if "which wkhtmltopdf"
     action :create_if_missing
-    notifies :install, 'ruby_block[install-wkhtmltopdf]', :immediately
+    notifies :run, 'ruby_block[install-wkhtmltopdf]', :immediately
 end
