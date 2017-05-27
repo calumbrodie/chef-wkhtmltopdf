9220ff4982a976851f310f20bd5c04a19f387ab0
Gavin Staniforth <gsdevme@users.noreply.github.com>
2017-05-27 15:19:34 +0100

    chore: support CentOS 6 & 7

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,5 +13,5 @@ remote_file "#{Chef::Config[:file_cache_path]}/wkhtmltox-#{node[:wkhtmltopdf][:v
     source "#{node[:wkhtmltopdf][:link]}"
     not_if "which wkhtmltopdf"
     action :create_if_missing
-    notifies :install, "package[#{Chef::Config[:file_cache_path]}/wkhtmltox-#{node[:wkhtmltopdf][:version]}_linux-centos6-amd64.rpm]", :immediately
+    notifies :install, "package[#{Chef::Config[:file_cache_path]}/wkhtmltox-#{node[:wkhtmltopdf][:version]}_linux-centos#{node['platform_version'].to_i}-amd64.rpm]", :immediately
 end
