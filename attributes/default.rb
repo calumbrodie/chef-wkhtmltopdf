933b744ec5e8f3dd2f9fbc3a5d8b0cb6903fa987
Gavin Staniforth <gsdevme@users.noreply.github.com>
2017-05-27 15:18:32 +0100

    chore: use downloads.wkhtmltopdf.org

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default[:wkhtmltopdf][:version] = "0.12.2"
 default[:wkhtmltopdf][:folder] = "0.12"
-default[:wkhtmltopdf][:host] = "download.gna.org"
-default[:wkhtmltopdf][:link] = "http://#{node[:wkhtmltopdf][:host]}/wkhtmltopdf/#{node[:wkhtmltopdf][:folder]}/#{node[:wkhtmltopdf][:version]}/wkhtmltox-#{node[:wkhtmltopdf][:version]}_linux-centos6-amd64.rpm"
+default[:wkhtmltopdf][:host] = "downloads.wkhtmltopdf.org"
+default[:wkhtmltopdf][:link] = "http://#{node[:wkhtmltopdf][:host]}/#{node[:wkhtmltopdf][:folder]}/#{node[:wkhtmltopdf][:version]}/wkhtmltox-#{node[:wkhtmltopdf][:version]}_linux-centos6-amd64.rpm"
 
