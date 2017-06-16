b35b6d43e039794d0d65a01a485ee01084a573d6
Gavin Staniforth <gsdevme@users.noreply.github.com>
2017-06-16 08:54:10 +0100

    chore: use github for a mirror

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default[:wkhtmltopdf][:version] = "0.12.2"
 default[:wkhtmltopdf][:folder] = "0.12"
-default[:wkhtmltopdf][:host] = "downloads.wkhtmltopdf.org"
+default[:wkhtmltopdf][:host] = "https://github.com/usemarkup/wkhtmltopdf-rpm/raw/master"
 default[:wkhtmltopdf][:link] = "http://#{node[:wkhtmltopdf][:host]}/#{node[:wkhtmltopdf][:folder]}/#{node[:wkhtmltopdf][:version]}/wkhtmltox-#{node[:wkhtmltopdf][:version]}_linux-centos#{node['platform_version'].to_i}-amd64.rpm"
 
